fbe1d89f5e0be2ade27d40fe3e9ac842e0192502
Salah Alomari <s.z.alomari.1987@gmail.com>
2016-07-20 13:25:35 +0400

    Build page for many entries

--- server/server.js
+++ server/server.js
@@ -10,12 +10,16 @@ var app = express();
 //mongoose.connect('mongodb://localhost/iGrow');
 
 // configure our server with all the middleware and routing
+
 require('./config/middleware.js')(app, express);
 require('./config/routes.js')(app, express);
 
 // start listening to requests on port 8000
-/*app.get('/',function(req,res){
-	res.redirect('./public')
-});*/
+
 app.listen(8000);
+
+app.listen(8000, function () {
+  console.log(' app listening on port 8000!');
+});
+
 module.exports = app;
