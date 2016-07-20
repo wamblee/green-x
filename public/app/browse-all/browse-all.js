c395c9e3fa043e456253a6b170b02cba9b860105
Salah Alomari <s.z.alomari.1987@gmail.com>
2016-07-20 21:37:51 +0400

    Build NgRepeat with Bootstrap

--- public/app/browse-all/browse-all.js
+++ public/app/browse-all/browse-all.js
@@ -1,4 +1,8 @@
-angular.module('iGrow.plants', [])
+angular.module('Myplants', [])
 .controller('BrowseController', function ($scope, Plants) {
-	
+	$scope.data.plants = [
+	{img: 'https://vienna-wv.com/images/tree.jpg', id:1, name:"Orcidia"},
+	{img: 'https://vienna-wv.com/images/tree.jpg', id:2, name:"Wisteria"},
+	{img: 'https://vienna-wv.com/images/tree.jpg', id:3, name:"Maple"}
+	];
 });
