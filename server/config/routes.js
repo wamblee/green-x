b82483fc0620fc0dc00c605356eea7408afcecb9
rana1926 <rana.kelani@gmail.com>
2016-07-21 19:59:32 +0300

    Add routes for the current controllers

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,19 +1,21 @@
-// var plantsController = require('../plants/plantsController.js');
+var plantsController = require('../plants/plantsController.js');
 var usersController = require('../users/usersController.js');
 var helpers = require('./helpers.js'); // our custom middleware
 
 module.exports = function (app, express) {
   // app.get('/:code', plantsController.navToLink);
-
   app.post('/api/users/signin', usersController.signin);
   app.post('/api/users/signup', usersController.signup);
   app.get('/api/users/signedin', usersController.checkAuth);
-  app.post('/api/users/newplant', usersController.addPlant);
+  app.post('/api/users/addplant', usersController.addPlant);
+  app.post('/api/users/newplant', plantsController.newPlant)
+  app.get('/api/plants/', plantsController.allPlants);
+  app.get('/api/users/garden', usersController.getGarden);
+  // app.put('/api/users/removeplant', usersController.removePlant);//TODO 
 
 
   // authentication middleware used to decode token and made available on the request
   // app.use('/api/plants', helpers.decode);
-  // app.get('/api/plants/', plantsController.allplants);
 
   app.use(helpers.errorLogger);
   app.use(helpers.errorHandler);
