0ae4ae2457cf5072c8e9aab13ea87da1b933e38a
Salah Alomari <s.z.alomari.1987@gmail.com>
2016-07-22 12:08:21 +0400

    Add basic controller

--- public/app/browse-all/browse-all.js
+++ public/app/browse-all/browse-all.js
@@ -1,5 +1,6 @@
-angular.module('Myplants', [])
-.controller('BrowseController', function ($scope, Plants) {
+angular.module('users.browse', [])
+.controller('BrowseController', function ($scope) {
+	$scope.data = {}
 	$scope.data.plants = [
 	{img: 'https://vienna-wv.com/images/tree.jpg', id:1, name:"Orcidia"},
 	{img: 'https://vienna-wv.com/images/tree.jpg', id:2, name:"Wisteria"},
