9868ab23e62fcf72c326f48be4b6b639a37fbf67
emano93 <emanothman506@gmail.com>
2016-07-22 17:07:22 +0300

    adding comments to usersModel,plantsModel,userscontroller addingit dbconfig file to server file

--- server/server.js
+++ server/server.js
@@ -1,21 +1,27 @@
 var express = require('express');
 var mongoose = require('mongoose');
-var db = require('./dbConfig');
 var User=require('./users/usersModel');
 var Plant=require('./plants/plantsModel')
 
+// connect to mongo database named "iGrow"
+var mongoURL = process.env.MONGODB_URI || 'mongodb://127.0.0.1:/iGrow';
+//create mongoose connection
+mongoose.connect(mongoURL);
+var db = mongoose.connection;
+// when there is an error with the connection
+db.on('error',function(err){
+ throw err;
+});
+// when the connection works 
+db.once('open',function(){
+ console.log('conecction is open');
+});
 var app = express();
-
-// connect to mongo database named "shortly"
-//mongoose.connect('mongodb://localhost/iGrow');
-
 // configure our server with all the middleware and routing
 require('./config/middleware.js')(app, express);
 require('./config/routes.js')(app, express);
-
 // start listening to requests on port 8000
-app.get('/',function(req,res){
-	res.redirect('./public')
-});
 app.listen(8000);
 module.exports = app;
+module.exports = db;
+
