7ac6c45b5f9696ac29399d205998ad9d1b4daf41
Salah Alomari <s.z.alomari.1987@gmail.com>
2016-07-22 13:25:32 +0400

    Handle server crash

--- server/server.js
+++ server/server.js
@@ -16,10 +16,23 @@ require('./config/routes.js')(app, express);
 
 // start listening to requests on port 8000
 
-app.listen(8000);
-
 app.listen(8000, function () {
   console.log(' app listening on port 8000!');
 });
 
+//Kills server connection if it crashes or killed
+//this is important so not to keep the 8000 port busy
+//If the app crashes
+app.on('uncaughtException', function(){
+	//Close connection
+	server.close();
+})
+// On kill
+app.on('SIGTERM', function(){
+	server.close();
+})
+//On exit
+app.on('exit', function(){
+	server.close();
+})
 module.exports = app;
