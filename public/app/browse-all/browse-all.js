75f63d4404c2fe5425118e4a4be245927d0e648d
Salah Alomari <s.z.alomari.1987@gmail.com>
2016-07-23 03:26:40 +0400

    Fix layout

--- public/app/browse-all/browse-all.js
+++ public/app/browse-all/browse-all.js
@@ -1,16 +1,17 @@
 angular.module('iGrow.browse', [])
-.controller('BrowseController', function ($scope, Plants) {
+.controller('BrowseController', function ($window, $scope, Plants) {
 	$scope.data = {}
-	$scope.data.plants = [
-	{img: 'https://vienna-wv.com/images/tree.jpg', id:1, name:"Orcidia"},
-	{img: 'https://vienna-wv.com/images/tree.jpg', id:2, name:"Wisteria"},
-	{img: 'https://vienna-wv.com/images/tree.jpg', id:3, name:"Maple"},
-	{img: 'https://vienna-wv.com/images/tree.jpg', id:4, name:"a"},
-	{img: 'https://vienna-wv.com/images/tree.jpg', id:5, name:"v"},
-	{img: 'https://vienna-wv.com/images/tree.jpg', id:6, name:"x"},
-	{img: 'https://vienna-wv.com/images/tree.jpg', id:7, name:"h"},
-	{img: 'https://vienna-wv.com/images/tree.jpg', id:8, name:"y"},
-	{img: 'https://vienna-wv.com/images/tree.jpg', id:9, name:"l"}
-
-	];
+	Plants.getAll()
+	.then(function(resp){
+		
+		console.log($window.localStorage["com.iGrow"])
+		$scope.data.plants=resp;
+	})
+	$scope.addTree = function (id){
+		Plants.AddPlant(id)
+		.then(function(resp){
+			console.log(resp)
+		})
+	}
+	
 });
