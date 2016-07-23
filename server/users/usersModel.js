d2f8d6c8a6e84805d3c09d8f24a61d3dcd6968a1
rana1926 <rana.kelani@gmail.com>
2016-07-23 10:04:42 +0300

    Heroku

--- server/users/usersModel.js
+++ server/users/usersModel.js
@@ -2,7 +2,6 @@ var Q = require('q');
 var mongoose = require('mongoose');
 var bcrypt = require('bcrypt-nodejs');
 var SALT_WORK_FACTOR = 10;
-var db = require('../dbConfig.js')
 var Plant = require('../plants/plantsModel')
 
  //creating usermodel and connecting it with plant table
