65d5335e114a0cf2ee4061714ed83f56f8f8df2a
Mohammad Albakri <mohammad.albakri93@gmail.com>
2016-07-28 19:33:52 +0300

    Clean up wrong merge with adding a GOOGLE MAP

--- public/app/stores/store.js
+++ public/app/stores/store.js
@@ -7,10 +7,41 @@ angular.module('iGrow.stores',[])
           zoom: 8,
           mapTypeId:  google.maps.MapTypeId.ROADMAP
         });
+
+         var infoWindow = new google.maps.InfoWindow({map: $scope.map});
+
+        // Try HTML5 geolocation.
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
+
+            
+            console.log(pos);
+
+            var info = 'Store Name :' + 'masthal' + '<br>'+ 'Number : ' + '09234\n'
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent(info);
+            $scope.map.setCenter(pos);
+          }, function() {
+            handleLocationError(true, infoWindow, $scope.map.getCenter());
+          });
+        } else {
+          // Browser doesn't support Geolocation
+          handleLocationError(false, infoWindow, $scope.map.getCenter());
+        }
+
+     }
+
+      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+                              'Error: The Geolocation service failed.' :
+                              'Error: Your browser doesn\'t support geolocation.');
       }
+
       initMap();
-
-})
-
-
-
+});
