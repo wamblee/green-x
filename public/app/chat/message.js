1e86855b1e62da3286cbe1d814bffc1a84375ef4
Mohammad Albakri <mohammad.albakri93@gmail.com>
2016-07-30 00:39:37 +0300

    Updated the messages with socket io to work efficiently

--- public/app/chat/message.js
+++ public/app/chat/message.js
@@ -2,15 +2,52 @@ angular.module('iGrow.What', [])
 .controller('socketController',function ($window, $location, $scope, Plants, socket) {
 
 	$scope.msgs = [];
+	$scope.users={};
+	$scope.testing = '';
+	socket.on('get msg' , function(data){
+		console.log('Came here');
+		console.log(data);
+		$scope.msgs.push(data);
+		$scope.$digest();
+	});
+	if($window.localStorage.getItem('user') === 'store'){
+		socket.emit('login' , {user : 'store' , name : $window.localStorage.getItem('com.storename')})
+	} else {
+		socket.emit('login' , {user : 'customer', name : $window.localStorage.getItem('com.username')})
+		console.log('Done this');
+	}
+
+	socket.on('users' , function(data){
+		$scope.users = data;
+		$scope.$digest();
+		console.log($scope.users);
+	})
 	
+	$scope.msg = {};
+	$scope.msg.username = $window.localStorage.getItem('com.username');
 	$scope.sendMsg = function(){
-		socket.emit('send msg' , $scope.msg.text)
+		socket.emit('send msg' , $scope.msg)
 		$scope.msg.text = "";
 	}
 
-	socket.on('get msg' , function(data){
-		$scope.msgs.push(data);
+
+	socket.on('user left' , function(data){
+		console.log('1 Left');
+		console.log(data);
+		$scope.users = data.users ;
+		$scope.message = data.string;
 		$scope.$digest();
-	})
-	console.log($window.localStorage.getItem('com.username'));
+	});
+
+	$scope.test = function(event){
+   	  Plants.selectStore(event)
+      .then(function (data){
+      	console.log(data,"my dataaaaaaaaaaaaaaaa")
+        $location.path('/'+event);
+
+      })
+      .catch(function (error){
+        console.log(error);
+      })
+	}
 });
