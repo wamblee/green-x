7e0a37ed0778ae46bc258e9cd37bf98062bfbd82
Mohammad Albakri <mohammad.albakri93@gmail.com>
2016-07-30 00:40:05 +0300

    Added All the connection with ons and emits at the server for socket io

--- server/server.js
+++ server/server.js
@@ -35,9 +35,66 @@ server.listen(port, function () {
 });
 
 
+var users = {
+	customers : {
+		names : [],
+		counter : 0
+	} , 
+	stores : {
+		names : [] ,
+		counter : 0
+	}
+}
+var user;
 io.sockets.on('connection' , function(socket){
+	console.log('Connected');
+	socket.on('login' , function(data){
+		// holds data.user && data.name
+		if(users.customers.names.indexOf(data.name) > -1 || users.stores.names.indexOf(data.name) > -1){
+			io.sockets.emit('users', users);
+			socket.broadcast.emit('users', users);
+			console.log('Not gonna Add you');
+		} else {
+			if(data.user === 'store'){
+				user = 'store';
+				socket.username = data.name;
+				users.stores.names.push(data.name);
+				users.stores.counter++;
+			} else {
+				user = 'customer'
+				socket.username = data.name;
+				users.customers.names.push(data.name);
+				users.customers.counter++;
+			}
+			console.log(users);
+			console.log('reached here');
+			io.sockets.emit('users', users);
+			socket.broadcast.emit('users', users);
+		}
+
+	})
+
 	socket.on('send msg' , function(data){
-		io.sockets.emit('get msg' , data);
+		console.log(data);
+		io.sockets.emit('get msg', data);
+		// socket.broadcast.emit('get msg' , data);
+	})
+
+
+	socket.on('disconnect',function(){
+		var string = socket.username + ' Just Left';
+		if(user === 'store'){
+			users.stores.names.splice(users.stores.names.indexOf(socket.username),1);
+			--users.stores.counter;
+		} else {
+			users.customers.names.splice(users.customers.names.indexOf(socket.username),1);
+			--users.customers.counter;
+		}
+		console.log(string);
+		var object =  {users : users , string : string} ;
+		io.sockets.emit('user left', object);
+		// socket.broadcast.emit('user left', users);
+		socket.broadcast.emit('user left', object);
 	})
 })
 
